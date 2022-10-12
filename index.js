ee766d568cdc957b41273a82828d3c0c8c4c0b89
nayr <ryan.syed1305@gmail.com>
2022-10-12 12:04:05 -0500

    Scaffolded initial design of API with comments
    Also added landing page for educational purposes :)

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const app = express();
+const convert = require("./lib/conversions")
 
 app.use(express.static(__dirname + '/client'))
 
-const port = process.env.PORT || 3000
-app.get('/test', function(request, response) {
-	response.type('text/plain')
-	response.send('Node.js and Express running on port='+port)
-})
+const port = process.env.PORT || 1337
+
+// Health check, verify that the server is running
+app.get('/api/health', function(request, response) {
+	response.type('application/json');
+	if (typeof port === 'string' || port instanceof String){
+		var res = '{"msg": "Node.js and Express running", "port": "' + port.replace(/\\/g, "\\\\") + '"}';
+	} else {
+		var res = '{"msg": "Node.js and Express running", "port": "' + port + '"}';
+	}
+	response.send(res);
+});
+
+app.get('/api/bmi', function(request, response) {
+	response.type('application/json');
+	// TODO: Accept JSON post request and then return calculated BMI
+});
+
+app.get('/api/risk', function(request, response) {
+	response.type('application/json');
+	// TODO: Accept the following inputs in JSON post
+	//		- age (int), BMI (calculated from /api/bmi), 
+	//		  blood pressure (string), family disease (list)
+	// Then use the point values assigned in the requirements to return
+	// a json object of {"risk": low|moderate|high|uninsurable, "score": SCORE}
+});
 
 app.listen(port, function() {
-	console.log("Server is running at http://localhost:3000/")
+	console.log("Server is running at http://localhost:"+port)
 })
